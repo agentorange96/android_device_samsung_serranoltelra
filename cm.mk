8afe506f3243182219a47253d5f58501c72bc9d1
agentorange96 <agentorange96@gmail.com>
2017-04-15 01:19:32 -0400

    Change for LRA

--- cm.mk
+++ cm.mk
@@ -4,7 +4,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-$(call inherit-product, device/samsung/serranoltelra/full_serranolteusc.mk)
+$(call inherit-product, device/samsung/serranoltelra/full_serranoltelra.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=serranoltelra TARGET_DEVICE=serranoltelra BUILD_FINGERPRINT="samsung/serranoltelra/serranoltelra:4.4.2/KOT49H/R890TYUBNE4:user/release-keys" PRIVATE_BUILD_DESC="serranoltelra-user 4.4.2 KOT49H I435LWWUBNK1 release-keys"
 
